2b63a05370a53554337b42a83b5d6b9b531bde0b
musti0811 <145289311+musti0811@users.noreply.github.com>
2024-06-11 21:54:20 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 class Config:
     MYSQL_HOST = 'localhost'
     MYSQL_USER = 'root'
-    MYSQL_PASSWORD = 'Aja62vedboss.dk'
+    MYSQL_PASSWORD = 'password'
     MYSQL_DB = 'registreringdb'
     MYSQL_CURSORCLASS = 'DictCursor'
